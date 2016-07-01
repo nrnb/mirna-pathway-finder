82818f779f061f7ea658415c372ae50ddb429de7
Anders Riutta <git@andersriutta.com>
2016-07-01 16:50:27 -0700

    Not sure why these changes were not committed

--- mirnapathwayfinder/__init__.py
+++ mirnapathwayfinder/__init__.py
@@ -2,45 +2,50 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import inspect
 from mirna_pathway_finder import MirnaPathwayFinder
+
+arg_spec = inspect.getargspec(MirnaPathwayFinder)
+arg_defaults = dict(zip(reversed(arg_spec.args), reversed(arg_spec.defaults)))
+default_column_number = arg_defaults['query_value_list_column_index'] + 1
 
 
 def main():
-
-    # TODO update the input args to whatever we actually need
-
     parser = argparse.ArgumentParser(
-        description='''Given a list of mirbase names, return an HTML page with a list of matching hits from WikiPathways.''')
+        description='''Given a list of mirbase names,
+        return an HTML page with a list of matching hits from WikiPathways.''')
     parser.add_argument('ids',
                         type=str,
                         help='identifier or file path to identifier list')
     parser.add_argument('-c', '--column',
-                        default=1,
+                        default=default_column_number,
                         type=int,
                         help='''column number for identifiers in identifier list file
-                        (default = 1)''')
+                        (default = {0})'''.format(default_column_number))
     parser.add_argument('-s', '--source',
                         help='''source file path with mappings from
                         WikiPathways identifiers to contained miRNAs.
-                        (default = file named "wp-mir-table-hs.csv"
-                            in top level of this project directory)''')
+                        (default = {0})
+                        '''.format(arg_defaults['mappings_path']))
     parser.add_argument('-t', '--type',
                         default='rna',
-                        help='input type (rna or protein; default = rna)')
+                        help='''input type (rna or protein; default = {0})
+                        '''.format(arg_defaults['node_type']))
     parser.add_argument('-o', '--output',
                         default='.',
                         help='''output directory path
                         (default = current working directory)''')
     parser.add_argument('--cache',
-                        default=True,
+                        default=arg_defaults['cache'],
                         type=bool,
                         help='''Cache pathway to miRNA mappings and use in
                         subsequent runs to reduce parse time
-                        (default = True)''')
+                        (default = {0})'''.format(arg_defaults['cache']))
     parser.add_argument('-d', '--debug',
-                        default=False,
+                        default=arg_defaults['debug'],
                         type=bool,
-                        help='Show debug messages (default = False)')
+                        help='''Show debug messages (default = {0})
+                        '''.format(arg_defaults['debug']))
     args = parser.parse_args()
 
     query_values = args.ids
