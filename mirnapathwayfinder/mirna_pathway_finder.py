dd7f6833c81aae1e98c19d32d41ad19926dab30a
Anders Riutta <git@andersriutta.com>
2016-07-05 22:04:16 -0700

    Better handling for miRNAs specified as genes and for targets.

--- mirnapathwayfinder/mirna_pathway_finder.py
+++ mirnapathwayfinder/mirna_pathway_finder.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import csv
+from functional import seq
+from math import fsum
 import os
 import pystache
 import re
@@ -18,6 +20,9 @@ def MirnaPathwayFinder(
         cache=True,
         debug=False):
 
+    def parse_hits_field(field):
+        return filter(lambda x: x != '', field.split(','))
+
     def print_debug(message):
         if debug:
             print message
@@ -26,32 +31,81 @@ def MirnaPathwayFinder(
     if mappings_path is None:
         mappings_path = os.path.join(current_path, '..', 'wp-mir-table-hs.csv')
 
+    # Get mappings from gene to miRNA
+    gene_mir_map_path = os.path.join(
+        current_path, '..', 'wp-mir-table-builder', 'gene-mir-map.txt'
+    )
+    gene_to_mirna_mappings = {}
+    with open(gene_mir_map_path, 'rU') as csvfile:
+        gene_to_mirna_mappings_reader = csv.DictReader(
+            csvfile, delimiter='\t')
+        for row in gene_to_mirna_mappings_reader:
+            mirna = row['miRBase ID(s)']
+            gene = row['EntrezGene ID']
+            if (mirna is not None) and (gene is not None):
+                gene_to_mirna_mappings[gene] = mirna
+
+    # Get gene targets for each miRNA
+    mir_gene_targets_path = os.path.join(
+        current_path, '..', 'wp-mir-table-builder', 'mir-gene-targets.txt'
+    )
+    targeted_genes_by_mirna = {}
+    targeted_genes_by_mirna_handled_mirnas = set()
+    targeted_genes_by_mirna_entries = set()
+    with open(mir_gene_targets_path, 'rU') as csvfile:
+        targeted_genes_by_mirna_reader = csv.DictReader(
+            csvfile,
+            delimiter='\t'
+        )
+        for row in targeted_genes_by_mirna_reader:
+            mirna = row['miRNA']
+            gene = row['target']
+            if mirna not in targeted_genes_by_mirna_handled_mirnas:
+                targeted_genes_by_mirna_handled_mirnas.add(mirna)
+                targeted_genes_by_mirna[mirna] = set()
+
+            key = (mirna + gene)
+            if key not in targeted_genes_by_mirna_entries:
+                targeted_genes_by_mirna_entries.add(key)
+                targeted_genes_by_mirna[mirna].add(gene)
+
+    targeted_genes_by_mirna_keys = targeted_genes_by_mirna.keys()
+
     # parse wp-mir-table-hs.csv (or other file, if specified)
     # to get mappings between pathways and mirnas,
     # including for each pathway:
-    # * gene_hits: targeted genes
-    # * mirna_hits: miRNAs actually existing in pathway
-    # * targeting_mirna_hits: miRNAs actually existing in the pathway or
-    #       inferred to exist in the pathway because they target genes or
-    #       proteins that actually exist in the pathway
+    # * genes: all gene products in the pathway, annotated as genes
+    # * mirna_hits_as_gene_specified: miRNAs actually existing in pathway,
+    #   annotated as genes
+    # * mirna_hits_as_mirna_specified: miRNAs actually existing in pathway,
+    #   annotated as miRNAs
+    # * mirna_hits_as_mirna_inferred: miRNAs NOT actually specified on the
+    #       pathway but
+    #       inferred to exist on the pathway because they target genes or
+    #       proteins that DO actually exist on the pathway
     pathway_to_mirna_mappings = mappings_path
     pathway_to_mirna_mappings_list = []
     if os.path.isfile(pathway_to_mirna_mappings):
         with open(pathway_to_mirna_mappings, 'rb') as csvfile:
             pathway_to_mirna_mappings_reader = csv.DictReader(csvfile)
             for row in pathway_to_mirna_mappings_reader:
-                gene_hits = row['ghits'].split(',')
-                mirna_hits = row['mhits'].split(',')
-                targeting_mirna_hits = row['mthits'].split(',')
+                genes = parse_hits_field(row['genes'])
+                mirna_hits_as_gene_specified = parse_hits_field(row['ghits'])
+                mirna_hits_as_mirna_specified = parse_hits_field(row['mhits'])
+                mirna_hits_as_mirna_inferred = parse_hits_field(row['mthits'])
+
                 wp_identifier = re.search('WP\d+', row['link']).group(0)
                 parsed_row = {
                     'name': row['name'],
                     'identifier': wp_identifier,
                     'id': row['link'],
-                    'gene_hits': gene_hits,
-                    'mirna_hits': mirna_hits,
-                    'targeting_mirna_hits':
-                        targeting_mirna_hits,
+                    'genes': genes,
+                    'mirna_hits_as_gene_specified':
+                        mirna_hits_as_gene_specified,
+                    'mirna_hits_as_mirna_specified':
+                        mirna_hits_as_mirna_specified,
+                    'mirna_hits_as_mirna_inferred':
+                        mirna_hits_as_mirna_inferred,
                 }
                 pathway_to_mirna_mappings_list.append(parsed_row)
     else:
@@ -60,23 +114,25 @@ def MirnaPathwayFinder(
         else:
             pathway_to_mirna_mappings_list.append(pathway_to_mirna_mappings)
 
-    query_value_list = []
+    query_value_list = set()
     if os.path.isfile(query_values):
         with open(query_values, 'rb') as csvfile:
             query_value_list_reader = csv.reader(
                 csvfile, delimiter='\t', quotechar='|')
             for row in query_value_list_reader:
-                query_value_list.append(row[query_value_list_column_index])
+                query_value_list.add(row[query_value_list_column_index])
     else:
         if hasattr(query_values, '__iter__'):
-            query_value_list += query_values
+            query_value_list = set(query_values)
         else:
-            query_value_list.append(query_values)
+            query_value_list.add(query_values)
 
     # TODO handle the case where the query values are NOT display names
-    # TODO use the gene_hits, mirna hits and mirna targets hits instead of
-    # just the query values to create this string
-    highlight_values = map(lambda query_value: 'label[]=' + urllib.quote(query_value), query_value_list)
+    highlight_values = map(
+        lambda query_value: 'label[]=' + urllib.quote(
+            query_value
+        ), query_value_list
+    )
     highlight_string = str.join('&', highlight_values) + '&colors=red'
 
     class MyObserver(Observer):
@@ -90,129 +146,182 @@ def MirnaPathwayFinder(
             print_debug("Sequence completed")
 
     def generate_widget_uri(mapping):
-        return 'http://www.wikipathways.org/wpi/PathwayWidget.php?id=' + mapping['identifier'] + '&' + highlight_string
+        return ''.join([
+            'http://www.wikipathways.org/wpi/PathwayWidget.php?id=',
+            mapping['identifier'],
+            '&',
+            highlight_string,
+        ])
 
-    def get_hits_and_counts(mapping):
-        # NOTE: we're not currently using the matching gene hits.
-        # They are intended to
-        # be used for specifying which node(s) to highlight,
-        # in the case that miRNAs
-        # are annotated with gene ids.
-        gene_query_value_list = set(mapping['gene_hits']).intersection(query_value_list)
-        mapping['gene_hits'] = map(lambda gene_hit: {'name': gene_hit}, gene_query_value_list)
-        mapping['gene_hit_count'] = len(mapping['gene_hits'])
+    def get_matches_and_counts(mapping):
+        # Find matching miRNAs in pathways, annotated as genes or miRNAs
+        mirna_hits_as_gene_specified = set(
+            mapping['mirna_hits_as_gene_specified'])
+        mirna_hits_as_mirna_specified = set(
+            mapping['mirna_hits_as_mirna_specified'])
 
-        mirna_query_value_list = set(mapping['mirna_hits']).intersection(query_value_list)
-        mapping['mirna_hits'] = map(lambda mirna_hit: {'name': mirna_hit}, mirna_query_value_list)
-        mapping['mirna_hit_count'] = len(mapping['mirna_hits'])
+        mirna_matches_as_gene_specified = query_value_list.intersection(
+            mirna_hits_as_gene_specified
+        )
+        mirna_matches_as_mirna_specified = query_value_list.intersection(
+            mirna_hits_as_mirna_specified
+        )
 
-        targeting_mirna_query_value_list = set(mapping['targeting_mirna_hits']).intersection(query_value_list)
-        mapping['targeting_mirna_hits'] = map(lambda targeting_mirna_hit: {'name': targeting_mirna_hit}, targeting_mirna_query_value_list)
-        mapping['targeting_mirna_hit_count'] = len(mapping['targeting_mirna_hits'])
+        mirna_matches_specified = mirna_matches_as_gene_specified.union(
+            mirna_matches_as_mirna_specified
+        )
+
+        mapping['mirna_matches_specified'] = map(
+            lambda mirna: {'name': mirna},
+            mirna_matches_specified
+        )
+
+        mapping['mirna_match_specified_count'] = len(
+            mapping['mirna_matches_specified']
+        )
+
+        # Find all miRNAs in pathways, including both those actually in
+        # the pathway and those that are inferred based on the
+        # presence of an miRNA target (a gene or protein) in the pathway
+        mirna_matches_as_mirna_inferred = query_value_list.intersection(
+            mirna_hits_as_mirna_inferred
+        )
+
+        mirna_matches_all = mirna_matches_as_mirna_specified.union(
+            mirna_matches_as_mirna_inferred
+        )
+
+        if len(mirna_matches_as_gene_specified) > 0:
+            # user specified desired miRNA(s) as gene(s), so
+            # we need to specify it as an miRNA so that we can
+            # look at the targeting
+            for gene in mirna_matches_as_gene_specified:
+                mirna_matches_all.add(gene_to_mirna_mappings[gene])
+
+        mapping['mirna_matches_all'] = map(
+            lambda mirna: {'name': mirna},
+            mirna_matches_all
+        )
+
+        mapping['mirna_match_all_count'] = len(
+            mapping['mirna_matches_all']
+        )
+
+        # Find miRNA targets (genes and proteins) in pathways
+        # TODO from mir-gene-targets.txt, get the gene targets.
+        # Then get the intersection between that result and genes
+
+        genes = set(mapping['genes'])
+
+        def create_target_title_string(mirna):
+            if mirna not in targeted_genes_by_mirna_keys:
+                return 'none'
+
+            targeted_genes = targeted_genes_by_mirna[mirna]
+            if targeted_genes is None:
+                return 'none'
+
+            targeted_genes_on_pathway = targeted_genes.intersection(genes)
+            if len(targeted_genes_on_pathway) == 0:
+                return 'none'
+
+            return ', '.join(
+                targeted_genes_on_pathway
+            )
+
+        if len(mirna_matches_all) > 0:
+            mapping['target_matches_by_mirna'] = (
+                seq(mirna_matches_all)
+                .map(lambda mirna: {
+                    'mirna': mirna,
+                    'targets': create_target_title_string(mirna)
+                })
+                )
+
+            target_matches_all = (
+                seq(mirna_matches_all)
+                .filter(lambda mirna: mirna in targeted_genes_by_mirna_keys)
+                .map(lambda mirna: targeted_genes_by_mirna[mirna])
+                .filter(lambda targets: hasattr(targets, '__iter__'))
+                .aggregate(set(), lambda acc, targets: acc.union(targets))
+                ).intersection(genes)
+        else:
+            target_matches_all = []
+
+        if len(target_matches_all) > 0:
+            mapping['target_matches'] = map(
+                lambda target_match: {'name': target_match},
+                target_matches_all
+            )
+        else:
+            mapping['target_matches'] = []
+
+        mapping['target_match_count'] = len(mapping['target_matches'])
 
         return mapping
 
-    def has_hit(mapping):
-        hit_count = mapping['gene_hit_count'] + mapping['mirna_hit_count'] + mapping['targeting_mirna_hit_count']
-        return hit_count > 0
+    def has_match(mapping):
+        match_count = fsum([
+            mapping['mirna_match_all_count'],
+            mapping['target_match_count'],
+        ])
+        return match_count > 0
 
-    table_template = '''<!DOCTYPE html>
-        <html>
-            <style>
-                body {
-                    font: 11px tahoma,arial,helvetica,sans-serif;
-                }
-                #pathway-to-mirna {
-                        font-family: "tahoma,arial,helvetica,sans-serif"
-                        font-size: 11px;
-                        background: none repeat scroll 0% 0% #FFF;
-                        width: 600px;
-                        border-collapse: collapse;
-                        text-align: left;
-                        margin: 20px;
-                }
-                #pathway-to-mirna th {
-                        border-bottom: 1px solid #AAA;
-                        padding: 0px 0px 3px;
-                }
-                #pathway-to-mirna tr:hover {
-                        background-color:#DDD;
-                }
-                a:link {
-                    text-decoration: none;
-                }
+    def sort_by_match_counts(mappings):
+        return sorted(
+            mappings,
+            key=lambda mapping: (
+                mapping['mirna_match_specified_count'],
+                mapping['mirna_match_all_count'],
+                mapping['target_match_count']
+            ),
+            reverse=True
+        )
 
-                a:visited {
-                    text-decoration: none;
-                }
-
-                a:hover {
-                    text-decoration: underline;
-                }
-
-                a:active {
-                    text-decoration: underline;
-                }
-                p {
-                        max-width:600px;
-                }
-            </style>
-            <body>
-                <h2>Pathway Finder</h2>
-                <p>The table lists pathways containing your miRNAs of interest and/or gene product targets of your miRNAs. The first column lists a clickable pathway title that updates the Interactive Pathway Viewer below. The second column lists pathway identifiers that link to WikiPathways.org. The third column shows the miRNA count in each pathway. The last column shows the count of the gene products targeted by miRNAs and, in parentheses, the count of the miRNAs doing the targeting.</p>
-                <p>The list is sorted by "miRNAs" (primary) and by "miRNA Targets" (secondary) found on each pathway. The results are limited to the top 20.</p>
-                <h3>Table of Pathway Results</h3>
-                <table id="pathway-to-mirna">
-                    <tr>
-                        <th>Pathway Title <i>(click to view pathway)</i></th>
-                        <th>Linkout</th>
-                        <th>miRNAs on Pathway</th>
-                        <th>Targets on Pathway (Targeting miRNAs)</th>
-                    </tr>
-                    {{#.}}<tr>
-                        <td id="name" title="view pathway"><a href="#wikipathways-widget-anchor">{{name}}</a></td>
-                        <td id="identifier" title="view pathway at WikiPathways.org"><a href="{{id}}" target="_blank">{{identifier}}</a></td>
-                        <td align="center" id="matching-mirna-hits" title="{{#mirna_hits}} {{name}}\n{{/mirna_hits}}">{{mirna_hit_count}}</td>
-                        <td align="center" id="matching-mirna-target-hits" title="{{#targeting_mirna_hits}} {{name}}\n{{/targeting_mirna_hits}}">0 ({{targeting_mirna_hit_count}})</td>
-                    </tr>{{/.}}
-                </table>
-                <a name="wikipathways-widget-anchor"></a>
-                <div id="wikipathways-widget">
-                  <h3>Interactive Pathway Viewer</h3>
-                    <iframe
-                        src="widget_uri"
-                        width="600px"
-                        height="300px"
-                        style="overflow:hidden;">
-                    </iframe>
-                </div>
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js"></script>
-                <script type="text/javascript">
-                    update_widget_string
-                </script>
-            </body>
-        </html>'''
-
-    def sort_by_hit_counts(mappings):
-        return sorted(mappings, key=lambda mapping: (mapping['mirna_hit_count'], mapping['targeting_mirna_hit_count']), reverse=True)
-
-    def take_top_hits(mappings):
+    def take_top_matches(mappings):
         return mappings[0:20]
 
     def generate_pathways_table(mappings):
+        f = open(current_path + '/table-template.html', 'r')
+        table_template = f.read()
         widget_uri = generate_widget_uri(mappings[0])
         initial_html_string = pystache.render(table_template, mappings)
-        html_string_with_widget_url = initial_html_string.replace('widget_uri', widget_uri)
+        html_string_with_widget_url = initial_html_string.replace(
+            'widget_uri',
+            widget_uri
+        )
 
         update_widget_path = os.path.join(current_path, 'update-widget.js')
         with open(update_widget_path, 'r') as update_widget:
-            update_widget_string = 'var highlightString = \'' + highlight_string + '\';\n' + update_widget.read()
-        html_string_with_update_widget = html_string_with_widget_url.replace('update_widget_string', update_widget_string)
+            update_widget_string = ''.join([
+                'var highlightString = \'',
+                highlight_string,
+                '\';\n',
+                update_widget.read()
+            ])
+
+        html_string_with_update_widget = html_string_with_widget_url.replace(
+            'update_widget_string',
+            update_widget_string
+        )
         f = open(os.path.join(output_dir, 'pathways.html'), 'w')
         f.write(html_string_with_update_widget)
         return html_string_with_update_widget
 
-    # Run to return result
+    # Run and generate HTML output
     mappings_source = Observable.from_(pathway_to_mirna_mappings_list)
-    mappings = mappings_source.map(get_hits_and_counts).filter(has_hit).to_list().map(sort_by_hit_counts).map(take_top_hits).map(generate_pathways_table)
+
+    mappings = mappings_source.map(
+        get_matches_and_counts
+    ).filter(
+        has_match
+    ).to_list(
+    ).filter(
+        lambda x: len(x) > 0
+    ).map(
+        sort_by_match_counts
+    ).map(
+        take_top_matches
+    ).map(generate_pathways_table)
+
     mappings.subscribe(MyObserver())
--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy==1.9.2', 'scipy==0.15.1', 'rx==1.2.3', 'pystache'],
+    install_requires=['numpy==1.9.2', 'scipy==0.15.1', 'rx==1.2.3', 'pystache', 'pyfunctional==0.7.1'],
 
     # List additional groups of dependencies here (e.g. development dependencies).
     # You can install these using the following syntax, for example:
